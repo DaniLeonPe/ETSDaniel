b1c8aa183802c2fd5fd7dd5e4ecdd7ed8f45debf
DaniLeonPe <dani.leonpe@gmail.com>
2023-06-06 15:28:54 +0100

    Se añade comentarios en CCuenta

--- Ets/src/main/java/cuentas/CCuenta.java
+++ Ets/src/main/java/cuentas/CCuenta.java
@@ -8,6 +8,9 @@ package cuentas;
  *
  * @author dam
  */
+/**
+ * Clase que representa una cuenta bancaria.
+ */
 public class CCuenta {
 
     private String nombre;
@@ -15,25 +18,54 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
-    public CCuenta()  {
+    /**
+     * Constructor por defecto de la clase CCuenta.
+     */
+    public CCuenta() {
     }
 
+    /**
+     * Constructor de la clase CCuenta.
+     *
+     * @param nom     El nombre del titular de la cuenta.
+     * @param cue     El número de cuenta.
+     * @param sal     El saldo inicial de la cuenta.
+     * @param tipo    El tipo de interés de la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
+        tipoInterés = tipo;
     }
 
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     *
+     * @return El saldo actual de la cuenta.
+     */
     public double estado() {
         return saldo;
     }
 
+    /**
+     * Ingresa una cantidad en la cuenta.
+     *
+     * @param cantidad La cantidad a ingresar.
+     * @throws Exception Si la cantidad es negativa.
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Retira una cantidad de la cuenta.
+     *
+     * @param cantidad La cantidad a retirar.
+     * @throws Exception Si la cantidad es negativa o no hay suficiente saldo.
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception("No se puede retirar una cantidad negativa");
